d9c05bb9025b048f6e8c1f00d755d4c5212e3865
Kailan B <kailanbates@gmail.com>
2024-06-05 20:32:08 -0600

    creating some classes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,44 @@
 ﻿
 
+
+class Connect4Board
+{
+    char[,] Board { get; private set; }
+
+    public Connect4Board()
+    {
+
+    }
+
+    public ResetBoard()
+    {
+        Board = new char[6, 7];
+        for (int i = 0; i < Board.GetLength(0))
+        {
+            for (int j = 0; j < Board.GetLength(1))
+            {
+                Board[i][j] = "#";
+            }
+        }
+    }
+
+    public DisplayBoard()
+    {
+        for (int i = 0; i < Board.GetLength(0))
+        {
+            for (int j = 0; j < Board.GetLength(1))
+            {
+                Console.WriteLine($"{Board[i, j], 2});   
+            }
+        }
+    }
+}
+
+
+
+
+
+
 class Program
 {
     static void Main(string[] args)
@@ -8,6 +47,6 @@ class Program
     }
 }
 
-//
 
-// Back with one more test XD
+
+
