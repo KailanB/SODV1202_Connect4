981c35a7031fdafdba319ed5047a69059c37605e
KailanB <kailanbates@gmail.com>
2024-06-05 21:00:06 -0600

    Update Program.cs
    Do not mind the last mess, I am still learning how to use git and properly open and upload programs.
    
    This update has a Board class, with a multidimensional array, the array is Reset with #'s in all spaces and has a method to display the array

--- Program.cs
+++ Program.cs
@@ -3,40 +3,37 @@
 
 class Connect4Board
 {
-    char[,] Board { get; private set; }
+    char[,] Board { get; set; }
 
     public Connect4Board()
     {
-
+        Board = new char[6, 7];
     }
 
-    public ResetBoard()
+    public void ResetBoard()
     {
         Board = new char[6, 7];
-        for (int i = 0; i < Board.GetLength(0))
+        for (int i = 0; i < Board.GetLength(0); i++)
         {
-            for (int j = 0; j < Board.GetLength(1))
+            for (int j = 0; j < Board.GetLength(1);  j++)
             {
-                Board[i][j] = "#";
+                Board[i, j] = '#';
             }
         }
     }
 
-    public DisplayBoard()
+    public void DisplayBoard()
     {
-        for (int i = 0; i < Board.GetLength(0))
+        for (int i = 0; i < Board.GetLength(0); i++)
         {
-            for (int j = 0; j < Board.GetLength(1))
+            for (int j = 0; j < Board.GetLength(1); j++)
             {
-                Console.WriteLine($"{Board[i, j], 2});   
+                Console.Write($"{Board[i, j], 2} ");   
             }
+            Console.WriteLine();
         }
     }
 }
-
-
-
-
 
 
 class Program
@@ -44,9 +41,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Team!");
+
+        Connect4Board board = new Connect4Board();
+        board.ResetBoard();
+        board.DisplayBoard();
+       
     }
 }
-
 
 
 
